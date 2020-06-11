2f103ddb08692ed3a6e67ac77c3eee2a9cc87e59
James Gallagher <james@Opencommit.com>
2020-06-11 18:56:22 +0100

    feat: Support upvotes

--- components/link.js
+++ components/link.js
@@ -1,12 +1,46 @@
 import React, { Component } from 'react'
 import { Box, Text, Anchor } from 'grommet'
+import gql from 'graphql-tag'
+import withApollo from '../lib/withApollo'
+import { useQuery } from '@apollo/react-hooks'
+import { Mutation } from 'react-apollo'
+
+const VOTE_MUTATION = gql`
+  mutation VoteMutation($linkId: ID!) {
+    createUpvote(linkId: $linkId) {
+      id
+      link {
+       id
+        upvotes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`
 
 class Link extends Component {
   render() {
     return (
       <div>
         <div style={{ paddingTop: 10, paddingBottom: 10 }}>
-          ▲ {this.props.link.upvotes.length} <Anchor href={this.props.link.url} color="gray">{this.props.link.title}</Anchor> {this.props.link.url}<br />
+          <Box direction="row">
+            <Mutation mutation={VOTE_MUTATION}
+            variables={{ linkId: this.props.link.id }}>
+              {voteMutation => (
+                <div className="ml1 gray f11" onClick={voteMutation} style={{ paddingRight: 10 }}>
+                  ▲ {this.props.link.upvotes.length}
+                </div>
+              )}
+            </Mutation>
+            <Anchor href={this.props.link.url} color="gray" style={{ paddingRight: 10 }}>{this.props.link.title}</Anchor> {this.props.link.url}<br />
+          </Box>
           <Anchor href={`/user/${this.props.link.by.id}`} color="gray">{this.props.link.by.username}</Anchor> 1 day ago <strong>{this.props.link.comments.length} comments</strong>
         </div>
       </div>
@@ -14,4 +48,4 @@ class Link extends Component {
   }
 }
 
-export default Link;
+export default withApollo(Link);
