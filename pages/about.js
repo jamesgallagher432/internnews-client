8fc755b078493685f383d440ceafeeb6663b7546
James Gallagher <james@Opencommit.com>
2020-06-12 15:04:40 +0100

    feat: Create new about page

--- pages/about.js
+++ pages/about.js
@@ -1,38 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Head from 'next/head'
 import Nav from '../components/nav'
-import { Grid, Box, Heading, Paragraph } from 'grommet'
-const About = () => (
-  <div>
-    <Head>
-      <title>About</title>
-      <link rel='icon' href='/favicon.ico' />
-    </Head>
-    <Grid
-      rows={['auto', 'xsmall']}
-      columns={['auto', 'auto']}
-      gap='small'
-      areas={[
-        { name: 'header', start: [0, 0], end: [1, 0] },
-        { name: 'nav', start: [0, 1], end: [0, 1] },
-        { name: 'main', start: [1, 1], end: [1, 1] }
-      ]}
-      fill='horizontal'
-    >
-      <Box gridArea='header' background='light-1'>
-        <Nav />
-      </Box>
-      <Box gridArea='nav' background='light-5' />
-      <Box gridArea='main'>
-        <Heading margin='none'>About</Heading>
-        <Paragraph>
-          This is a demo of Gromment working with next. The main reference for
-          this impelmentation include:
-          https://github.com/zeit/next.js/tree/master/examples/with-styled-components
-        </Paragraph>
-      </Box>
-    </Grid>
-  </div>
-)
+import LinkList from '../components/linklist'
+import { Box, Text, FormField, TextInput, Button, Anchor, Heading } from 'grommet'
+import styled from 'styled-components'
+import withApollo from '../lib/withApollo'
+import { Mutation } from 'react-apollo'
+import gql from 'graphql-tag'
 
-export default About
+const MainBox = styled(Box)`
+  padding-top: 20px;
+  padding-left: 5%;
+  padding-right: 5%;
+  min-height: 100vh;
+`;
+
+const POST_MUTATION = gql`
+  mutation SignInUser($email: String!, $password: String!) {
+    signinUser(
+      credentials: {
+        email: $email,
+        password: $password
+      }
+    ) {
+      token
+      user {
+        id
+      }
+    }
+  }
+`
+
+function About() {
+    return (
+      <div>
+        <Head>
+          <title>About | Intern News</title>
+          <link rel='icon' href='/favicon.ico' />
+        </Head>
+        <Nav />
+        <MainBox style={{ backgroundColor: "#F0F0F0" }}>
+          <Box style={{ backgroundColor: "white", padding: 20, borderRadius: 10 }} align="left">
+            <Heading level="2">Sign In</Heading>
+            <Text>Intern News is a news aggregator for interns and young people interested in tech.</Text>
+            <Heading level="6">The Team</Heading>
+            <Text>Intern News was built by <Anchor color="gray" href="https://jamesg.app">James Gallagher</Anchor>.</Text>
+          </Box>
+          </MainBox>
+      </div>
+    );
+};
+
+export default withApollo(About);
